4807d8e6155c0fd7e2f98a5682a19eba433f6b33
huangshaowu <huangshaowu@qianxin.com>
2022-10-28 16:08:47 +0800

    feat: 登录校验提示信息修改的提交

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -4,11 +4,11 @@ module.exports = options => {
     const assert = require('http-assert')
     return async (req, res, next) => {
         const token = String(req.headers.authorization || '').split(' ').pop() // pop()取数组最后一项
-        // assert(token, 401, '请提供jwt token')
-        assert(token, 401, '请先登录1')
+        assert(token, 401, '请提供jwt token')
+        // assert(token, 401, '请先登录1')
         const { id } = jwt.verify(token, req.app.get('secret')) //校验解码  req.app 等同于 express里的app
-        // assert(id, 401, '无效的jwt token')
-        assert(id, 401, '请先登录2')
+        assert(id, 401, '无效的jwt token')
+        // assert(id, 401, '请先登录2')
         req.user = await AdminUser.findById(id)
         assert(req.user, 401, '请先登录3')
         await next()
